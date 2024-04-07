e596c27f93e37d08e53d5f74a6641064f5265fd8
iamDavidSosa <a.davidsosa.t@gmail.com>
2024-04-07 16:09:24 -0400

    Le di enter

---
+++ CapaDatos/CD_Componentes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Componentes
+    {
+        private CD_Conexion conexion = new CD_Conexion();
+        private SqlDataReader leer;
+        private SqlCommand comando = new SqlCommand();
+
+        public DataTable Mostrar()
+        {
+            DataTable tabla = new DataTable();
+
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "MostrarComponentes";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            leer.Close();
+            conexion.CerrarConexion();
+            return tabla;
+        }
+
+        public void Insertar(string nombre, string descripcion, string marca, string modelo, string serie, string ubicacion, string estado)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "InsertarComponente";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@descripcion", descripcion);
+            comando.Parameters.AddWithValue("@marca", marca);
+            comando.Parameters.AddWithValue("@modelo", modelo);
+            comando.Parameters.AddWithValue("@serie", serie);
+            comando.Parameters.AddWithValue("@ubicacion", ubicacion);
+            comando.Parameters.AddWithValue("@estado", estado);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+        public void Editar(string nombre, string descripcion, string marca, string modelo, string serie, string ubicacion, string estado, int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EditarComponente";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@descripcion", descripcion);
+            comando.Parameters.AddWithValue("@marca", marca);
+            comando.Parameters.AddWithValue("@modelo", modelo);
+            comando.Parameters.AddWithValue("@serie", serie);
+            comando.Parameters.AddWithValue("@ubicacion", ubicacion);
+            comando.Parameters.AddWithValue("@estado", estado);
+            comando.Parameters.AddWithValue("@id", id);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+        public void Eliminar(int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "EliminarComponente";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", id);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+    }
+}
